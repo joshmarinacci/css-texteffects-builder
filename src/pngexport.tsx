dfd18e60e873ce4ab5f749f7c47e6b81768d4a23
Josh Marinacci <joshua@marinacci>
2023-06-21 12:38:24 -0700

    update png export to clip to the necessary spritesheet size

--- src/pngexport.tsx
+++ src/pngexport.tsx
@@ -1,67 +1,86 @@
 import {lerpHSL, objToHsla, Style} from "./model";
 import React, {useEffect, useRef, useState} from "react";
 import {HBox, VBox} from "josh_react_util";
-import {Bounds, lerp_number} from "josh_js_util";
+import {Bounds, lerp_number, Point} from "josh_js_util";
 import {canvas_to_blob, forceDownloadBlob} from "josh_web_util";
 
-function drawStyleToContext(style: Style, text: string, ctx: CanvasRenderingContext2D, bounds: Bounds) {
+function drawStyleToContext(style: Style, fullText: string, ctx: CanvasRenderingContext2D, bounds: Bounds, forExport:boolean) {
+    console.log("font size is",style.fontSize)
+    const size = style.fontSize
     ctx.save()
     ctx.fillStyle = objToHsla(style.backgroundColor)
     ctx.fillRect(bounds.x,bounds.y,bounds.w,bounds.h)
 
-    let c = bounds.left_midpoint()
-    let font = `${style.fontWeight} ${style.fontSize}px "${style.fontFamily}"`
-    // console.log('canvas font:',font)
-    ctx.font = font
-    ctx.textAlign = 'start'
 
-    if(style.strokeEnabled) {
-        let r = style.strokeWidth
-        let steps = 8.0
-        for(let i=0; i<steps; i++) {
-            let theta = i*(Math.PI*2/steps)
-            let x_off = r * Math.cos(theta)
-            let y_off = r * Math.sin(theta)
-            ctx.shadowOffsetX = x_off - bounds.w
-            ctx.shadowOffsetY = y_off
-            ctx.shadowBlur = 0
-            ctx.shadowColor = objToHsla(style.strokeColor)
-            ctx.fillStyle = 'red'
-            ctx.fillText(text,c.x+bounds.w,c.y+0)
-            ctx.shadowColor = 'transparent'
-        }
-    }
-    if(style.shadowEnabled) {
-        if (style.shadowGradientEnabled) {
-            for (let i = style.shadowGradientSteps; i>=0; i--) {
-                let t = i / style.shadowGradientSteps;
-                let color = lerpHSL(t, style.shadowColor, style.shadowEndColor)
-                let x_off = lerp_number(t, 0, style.shadowOffsetX)
-                let y_off = lerp_number(t, 0, style.shadowOffsetY)
-                let blur = lerp_number(t, 0, style.shadowBlurRadius)
+    function drawLetter(letter:string,bds:Bounds) {
+        ctx.save()
+        let font = `${style.fontWeight} ${style.fontSize}px "${style.fontFamily}"`
+        let pt = bds.bottom_midpoint()
+        ctx.font = font
+        ctx.textAlign = 'center'
+        if(style.strokeEnabled) {
+            let r = style.strokeWidth
+            let steps = 8.0
+            for(let i=0; i<steps; i++) {
+                let theta = i*(Math.PI*2/steps)
+                let x_off = r * Math.cos(theta)
+                let y_off = r * Math.sin(theta)
                 ctx.shadowOffsetX = x_off - bounds.w
                 ctx.shadowOffsetY = y_off
-                ctx.shadowBlur = blur
-                ctx.shadowColor = objToHsla(color)
+                ctx.shadowBlur = 0
+                ctx.shadowColor = objToHsla(style.strokeColor)
                 ctx.fillStyle = 'red'
-                ctx.fillText(text,c.x+bounds.w,c.y+0)
+                ctx.fillText(letter,pt.x+bounds.w,pt.y)
                 ctx.shadowColor = 'transparent'
             }
-        } else {
-            ctx.shadowColor = objToHsla(style.shadowColor)
-            // ctx.shadowColor = 'blue'
-            ctx.shadowOffsetX = style.shadowOffsetX-bounds.w
-            ctx.shadowOffsetY = style.shadowOffsetY
-            ctx.shadowBlur = style.shadowBlurRadius
-            ctx.fillStyle = 'red'
-            ctx.fillText(text,c.x+bounds.w,c.y+0)
-            ctx.shadowColor = 'transparent'
         }
+        if(style.shadowEnabled) {
+            if (style.shadowGradientEnabled) {
+                for (let i = style.shadowGradientSteps; i>=0; i--) {
+                    let t = i / style.shadowGradientSteps;
+                    let color = lerpHSL(t, style.shadowColor, style.shadowEndColor)
+                    let x_off = lerp_number(t, 0, style.shadowOffsetX)
+                    let y_off = lerp_number(t, 0, style.shadowOffsetY)
+                    let blur = lerp_number(t, 0, style.shadowBlurRadius)
+                    ctx.shadowOffsetX = x_off - bounds.w
+                    ctx.shadowOffsetY = y_off
+                    ctx.shadowBlur = blur
+                    ctx.shadowColor = objToHsla(color)
+                    ctx.fillStyle = 'red'
+                    ctx.fillText(letter,pt.x+bounds.w,pt.y+0)
+                    ctx.shadowColor = 'transparent'
+                }
+            } else {
+                ctx.shadowColor = objToHsla(style.shadowColor)
+                // ctx.shadowColor = 'blue'
+                ctx.shadowOffsetX = style.shadowOffsetX-bounds.w
+                ctx.shadowOffsetY = style.shadowOffsetY
+                ctx.shadowBlur = style.shadowBlurRadius
+                ctx.fillStyle = 'red'
+                ctx.fillText(letter,pt.x+bounds.w,pt.y+0)
+                ctx.shadowColor = 'transparent'
+            }
+        }
+        ctx.fillStyle = objToHsla(style.color)
+        ctx.fillText(letter,pt.x,pt.y)
+        ctx.restore()
     }
 
-    ctx.fillStyle = objToHsla(style.color)
-    ctx.fillText(text,c.x,c.y)
-
+    Array.from(fullText).forEach((letter,i) => {
+        // let pt = new Point(0+size*i,80)
+        let bds = new Bounds(0+size*i,size,size,size)
+        bds = bds.add(new Point(0,-size))
+        drawLetter(letter,bds)
+        if(!forExport) {
+            ctx.strokeStyle = 'black'
+            ctx.strokeRect(bds.x, bds.y, bds.w, bds.h)
+        }
+    })
+    if(!forExport) {
+        ctx.fillStyle = 'black'
+        ctx.font = '30px sans-serif';
+        ctx.fillText(`sprite size ${size}`,20,300-20)
+    }
 
     ctx.restore()
 }
@@ -74,12 +93,17 @@ export function PNGExportView(props: { style: Style }) {
         if(ref.current) {
             // @ts-ignore
             const ctx = ref.current.getContext('2d')
-            drawStyleToContext(props.style,text,ctx, new Bounds(0,0,1000,400))
+            drawStyleToContext(props.style,text,ctx, new Bounds(0,0,1000,500),false)
         }
     },[props.style,text])
     const exportCanvas = async () => {
         if (ref.current) {
-            let blob = await canvas_to_blob(ref.current)
+            const canvas = document.createElement('canvas')
+            canvas.width = props.style.fontSize * text.length
+            canvas.height = props.style.fontSize
+            let ctx = canvas.getContext('2d') as CanvasRenderingContext2D
+            drawStyleToContext(props.style,text,ctx, new Bounds(0,0,canvas.width,canvas.height),true)
+            let blob = await canvas_to_blob(canvas)
             forceDownloadBlob('output.png',blob)
         }
     }
